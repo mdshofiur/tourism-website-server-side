41426b84121f80cebb6e84adee3c1d01a28b5b0c
mdshofiur <shofiurbd13@gmail.com>
2021-10-30 12:31:27 +0600

    delete api

--- index.js
+++ index.js
@@ -65,7 +65,12 @@ async function run() {
 
   // Delete Api
 
-  
+  app.delete('/myorders/:id', async (req,res) => {
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await BookingCollection.deleteOne(query);
+    res.send(result);
+  })
 
 
 
